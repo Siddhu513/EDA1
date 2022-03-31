f0351aafcb59e300b7a4cefa9e08538fe25972f6
Siddhu513 <96914394+Siddhu513@users.noreply.github.com>
2022-03-31 14:14:14 +0530

    Initial commit

---
+++ plot1.R
@@ -0,0 +1,12 @@
+library(dplyr)
+
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+emissions_by_year <- NEI %>%
+  group_by(year) %>%
+  summarize(total_emissions = sum(Emissions))
+
+with(emissions_by_year, barplot(total_emissions, names.arg = year, xlab = "Year", ylab = "PM2.5 Emissions (tons)", main = "Total PM2.5 Emissions from All Sources"))
+
+dev.copy(png,'plot1.png')
